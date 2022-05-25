4c2cbcb8b7fb18ad6934eef5cf718d1b20c577c4
stompesi <stompesi@gmail.com>
2022-05-25 15:57:56 +0900

    Add loging in the batch_decrypt script

--- script/batch_decrypt.sh
+++ script/batch_decrypt.sh
@@ -3,7 +3,14 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 beginTime=$(date +%s%N)
 
-$SCRIPTPATH/../target/debug/crypto_cli --action-type batch_decrypt --batch-file-path $SCRIPTPATH/data/batch_decryption_info.data --use-thread true > $SCRIPTPATH/data/result.txt
+use_vdf_zkp=false
+use_encryption_zkp=false
+
+echo "### Start decryption batch###"
+echo "- Use vdf zkp: $use_vdf_zkp"
+echo "- Use encryption zkp: $use_encryption_zkp"
+
+$SCRIPTPATH/../target/debug/crypto_cli --use-vdf-zkp $use_vdf_zkp --use-encryption-zkp $use_encryption_zkp  --action-type batch_decrypt --batch-file-path $SCRIPTPATH/data/batch_decryption_info.data --use-thread true > $SCRIPTPATH/data/result.txt
 
 endTime=$(date +%s%N) 
 elapsed=`echo "($endTime - $beginTime) / 1000000" | bc` 
